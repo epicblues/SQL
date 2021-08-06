52c130304e64aa0d12b49bcdc1a9af201fa3fd34
KMS <epicblue@hanmail.net>
2021-08-06 17:48:52 +0900

    ~ TRANSACTION

--- empty.sql
+++ empty.sql
@@ -1,33 +1,104 @@
-CREATE ROLE C##babo;
-GRANT create session TO C##babo;
-GRANT select any table to C##babo;
-grant drop any table to C##babo;
-grant delete on hr.employees to C##babo; 
-grant connect, resource to C##babo;
-select * from role_sys_privs;
-select * from ROLE_TAB_PRIVs where role = 'C##BABO';
+select * from user_objects;
+select * from user_constraints;
+select * from user_tables;
+select * from user_objects;
+select * from all_users;
+delete from emp123;
+select * from user_tablespaces;
+select * from user_tablespaces;
+select * from user_tables;
+select * from all_users;
+select * from user_constraints where table_name = 'EMPLOYEES';
 
-alter user C##kmsbabo
-    default tablespace users
-    quota unlimited on users;
-    
-select * from user_tablespace;
-    
+select * from user_users;
+select * from user_tab_columns;
+INSERT INTO emp123 (select * from hr.employees);
+select * from emp123;
+ALTER TABLE emp123 drop (email,phone_number);
 
-CREATE USER C##kmsbabo identified by default;
-alter user C##kmsbabo identified by 1234;
+desc emp123;
+select * from user_constraints where table_name = 'EMP123';
+ALTER TABLE emp123 add constraint emp_pk PRIMARY KEY (employee_id);
 
-grant c##babo to c##kmsbabo;
+ALTER TABLE emp123 drop constraint emp_pk;
 
-revoke c##babo from c##kmsbabo;
+ALTER TABLE emp123 add constraint SYS_C009528;
 
-select * from DBA_USERS where USERname = 'C##KMSBABO';
+ALTER TABLE emp123 add (kmsbabo nvarchar2(20) DEFAULT 'babo' not null);
 
-SELECT * from USER_ROLE_PRIVS;
+select * from emp123;
 
-grant select on hr.employees to c##kmsbabo;
-select * from dba_TABLESPACEs;
-select * from role_sys_privs where role like '%conn%';
+desc emp123;
 
-drop role C##babo;
-revoke select on hr.employees from c##kmsbabo;
+select * from user_constraints where constraint_type = 'P';
+
+select * from user_primary_keys;
+
+select * from user_objects;
+
+select * from book;
+desc book;
+
+CREATE TABLE KMSGOD (
+    k_id number(20) not null,
+    k_game char(5),
+    k_name NVARCHAR2(20) default '바보' not null,
+    upload_date date default sysdate,
+    k_contents clob default '기본_내용입니다' not null,
+    constraint KMS_PK PRIMARY KEY(k_id),
+    constraint KMS_UNI UNIQUE(k_name)
+);
+
+desc kmsgod;
+
+INSERT INTO KMSGOD (k_id, k_game) values (12341, 'abra');
+select * from kmsgod;
+
+rename kmsgod to kmsbabo;
+
+select * from tabs;
+
+desc book;
+desc author;
+select * from user_constraints where table_name = 'BOOK';
+
+alter table book drop constraint FK_AUTHOR_ID;
+desc author;
+alter table book drop constraint fk_author_id;
+                        
+select * from book;
+desc book;
+INSERT INTO book values(1, 'hahaha',default , 1);
+INSERT INTO book values(2, 'hahoho',default , 2);
+INSERT INTO book values(3, USER, default ,2);
+UPDATE book set author_id = 2 where author_id is null;
+select * from book;
+select rowid from book;
+commit;
+
+select * from book;
+alter table book add primary key(author_id);
+select * from author;
+
+select * from author, book where book.author_id = author.author_id;
+
+desc hr.employees;
+
+select * from author;
+
+delete from author where author_id = 1;
+select * from book;
+
+alter table book drop primary key;
+
+select * from emp123;
+
+alter table emp123 drop column kmsbabo;
+
+INSERT INTO emp123 (
+    select employee_id, first_name, last_name, hire_date, job_id, salary, commission_pct, manager_id, department_id 
+    from hr.employees where salary = 6000
+);
+select * from user_tab_columns;
+
+select * from emp123 where salary = 6000;
