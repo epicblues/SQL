6a7590c74712085857f521179198454fdef5b3d4
KMS <epicblue@hanmail.net>
2021-08-10 21:47:16 +0900

    reviewed whole pdfs

--- MINI_PROJECT.sql
+++ MINI_PROJECT.sql
@@ -7,15 +7,9 @@ CREATE TABLE PHONE_BOOK (
 
 CREATE SEQUENCE seq_phone_book;
     
-drop Sequence seq_phone_book;
-
-    
 SELECT * FROM USER_SEQUENCES;
 
 SELECT * FROM phone_book;
 
 INSERT INTO phone_book values(seq_phone_book.nextval, '김민성','010-2406-5216','02-6242-5216');
 commit;
-
-select seq_phone_book.nextval from dual;
-select seq_phone_book.currval from dual;
